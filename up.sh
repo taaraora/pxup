9080de6e5124f1d2c295533e92e768c1109c96d5
Luis Pabón <luis@portworx.com>
2019-06-10 14:02:51 -0700

    save

--- up.sh
+++ up.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-. utils.sh
+. ./utils.sh
 
 pullimage=$(getvar pullimage)
 pximage=$(getvar pximage)
 
-if [ "pullimage" = "false" ] ; then
+if [ $pullimage = "false" ] ; then
+	echo "Copying docker ${pximage} ..."
 	docker save ${pximage} > $PWD/roles/common/files/px.img
 fi
 
